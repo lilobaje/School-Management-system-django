a406f0d1ed86a34b15c6292112e2db33d34cef65
John Obaje <67030349+lilobaje@users.noreply.github.com>
2022-07-15 16:08:22 -0700

    Update

---
+++ school_management_app/LoginCheckMiddleWare.py
@@ -0,0 +1,45 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.deprecation import MiddlewareMixin
+
+
+class LoginCheckMiddleWare(MiddlewareMixin):
+
+    def process_view(self,request,view_func,view_args,view_kwargs):
+        modulename=view_func.__module__
+        print(modulename)
+        user=request.user
+        if user.is_authenticated:
+            if user.user_type == "1":
+                if modulename == "school_management_app.adminviews" :
+                    pass
+                elif modulename == "school_management_app.views" or modulename == "django.views.static":
+                    pass               
+                elif modulename == "django.contrib.auth.views" or modulename =="django.contrib.admin.sites":
+                    pass
+
+                else:
+                    return HttpResponseRedirect(reverse("admin_home"))
+            elif user.user_type == "2":
+                if modulename == "school_management_app.StaffViews":
+                    pass
+                elif modulename == "school_management_app.views" or modulename == "django.views.static":
+                    pass
+                else:
+                    return HttpResponseRedirect(reverse("staff_home"))
+            elif user.user_type == "3":
+                if modulename == "school_management_app.StudentViews" or modulename == "django.views.static":
+                    pass
+                elif modulename == "school_management_app.views" :
+                    pass
+                else:
+                    return HttpResponseRedirect(reverse("student_home"))
+            else:
+                return HttpResponseRedirect(reverse("Logindon"))
+
+        else:
+            if request.path == reverse("Logindon") or request.path == reverse("do_login") or modulename == "django.contrib.auth.views": 
+                pass
+            else:
+                return HttpResponseRedirect(reverse("Logindon"))
+                
